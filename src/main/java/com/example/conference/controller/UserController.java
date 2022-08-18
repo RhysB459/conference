4daa806654dad653dd541439239055785192cc3b
rhys.ballinger <rhys.ballinger@outlook.com>
2022-08-18 15:46:10 +0100

    Completed Demo:@PostMapping

--- src/main/java/com/example/conference/controller/UserController.java
+++ src/main/java/com/example/conference/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.conference.controller;
 
 import com.example.conference.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,12 @@ public class UserController {
         user.setLastName(lastName);
         user.setAge(age);
 
+        return user;
+    }
+
+    @PostMapping("/user")
+    public User postUser(User user) {
+        System.out.println("User firstName: " + user.getFirstName());
         return user;
     }
 
